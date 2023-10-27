891b8bc52c17656fda65a889ed83e01356db20b3
Morry Kolman <morristskolman@gmail.com>
2023-10-27 09:13:43 -0400

    pdf fix

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This has been designed mainly to work with those sheet music channels like [musi
 
 ### Run
 
-Just drop your yt url into this command:
-`python convert_video_to_pdf.py "youtubeURLHere"`
-
-Currently outputs a long jpg, not a PDF. Working on that.
+1. Clone this repo
+2. Navigate into the directory on your CLI
+3. Drop your yt url into this command: `python convert_video_to_pdf.py "youtubeURLHere"`
+4. Your PDF will be in the `/output_pdfs/` directory
--- convert_video_to_pdf.py
+++ convert_video_to_pdf.py
@@ -1,3 +1,5 @@
+# TODO: add in controls for contrast control
+
 
 # Import everything needed to edit video clips 
 from moviepy.editor import *
@@ -12,6 +14,10 @@ from yt_dlp import YoutubeDL
 import sys
 import subprocess
 import shlex
+
+#pdf stuff
+from fpdf import FPDF
+pdf = FPDF()
 
 #gets the url you put in the command line
 video = sys.argv[1:][0]
@@ -35,7 +41,7 @@ video_file = title+".mp4"
 
 # this is to remove any weird contrast cuts
 # very hacky way to do this
-contrast_command = f'ffmpeg -i "{video_file}" -vf "hue=s=0,eq=contrast=1.4" "2{video_file}" -y'
+contrast_command = f'ffmpeg -i "{video_file}" -vf "hue=s=0,eq=contrast=1" "2{video_file}" -y'
 contrast_args = shlex.split(contrast_command)
 subprocess.run(contrast_args)
 copy_args = ['cp', str(2)+video_file, video_file]
@@ -78,9 +84,29 @@ for shot in scene_list:
     # final_image_name = "./classification_cache/frame"+str(i)+".jpg"
 
 
+# make a PDF
+
+# Make as many pages as needed to paste the frames into
+
+# figure out how many rows of images a width can suppport
+# width / height > 8.5/11
+
+# page_count = 1
+# current_page_height = 0
+# ratio = 0
+# width = final_images[0].size[0]
+# page_ratio = 8.5/11
+
+# for image in final_images:
+#   current_page_height = current_page_height + image[0].size[1]
+#   ratio = width / current_page_height
+#   if ratio > page_ratio:
+#     continue
+
 print("above width height")
 width = final_images[0].size[0]
-height = final_images[0].size[1] * len(final_images)
+# height = final_images[0].size[1] * len(final_images)
+height = int(final_images[0].size[1] * 2.3)
 print(width, height)
 
 new_im = Image.new('RGB', (width, height), "white")
@@ -99,11 +125,31 @@ font = ImageFont.truetype("Roboto-Medium.ttf", size=20)
 draw.text((10, 10), title, fill="black", font=font)
 
 y_offset = 100
+current_page = 0
+
+final_im_arr = [new_im]
 for im in final_images:
-  new_im.paste(im, (0, y_offset))
+  if (im.size[1] + y_offset) > final_im_arr[len(final_im_arr)-1].size[1]:
+    print("would make page too big, adding new page")
+    final_im_arr.append(Image.new('RGB', (width, height), "white"))
+    y_offset = 20
+  final_im_arr[len(final_im_arr)-1].paste(im, (0, y_offset))
+  # new_im.paste(im, (0, y_offset))
   y_offset += im.size[1]
 
-new_im.save(title + '.jpg')
+total_ims = 0
+for im in final_im_arr:
+    pdf.add_page()
+    im_path = title + str(total_ims) + '.jpg'
+    im.save(im_path)
+    pdf.image(im_path, 0, 0, 210, 297)  # Assumes A4 size for the PDF
+    total_ims += 1
+pdf.output('./output_pdfs/' + title + ".pdf")
+
+
+
+
+# new_im.save(title + '.jpg')
 
 
 # y_offset = 100
